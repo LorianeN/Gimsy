0ea9014a01b89c2b2b12b3e2a700f8373f2b4796
Nathan Laubeuf <nathanlaubeuf@MacBook-Pro-de-Nathan.local>
2015-12-03 23:32:01 +0100

    start

--- app/src/main/java/fred/gimsy/AccueilActivity.java
+++ app/src/main/java/fred/gimsy/AccueilActivity.java
@@ -1,7 +1,11 @@
 package fred.gimsy;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        Button selectButton = (Button) findViewById(R.id.selectButton);
     }
 }
