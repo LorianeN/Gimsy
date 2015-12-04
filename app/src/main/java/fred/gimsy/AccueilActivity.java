934a33701188c6ddd71b599928fc41df7dbc744f
Nathan Laubeuf <nathanlaubeuf@MacBook-Pro-de-Nathan.local>
2015-12-04 01:14:27 +0100

    corrected bis

--- app/src/main/java/fred/gimsy/AccueilActivity.java
+++ app/src/main/java/fred/gimsy/AccueilActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,7 +26,12 @@ public class AccueilActivity extends AppCompatActivity {
         newButton.setTypeface(typeFace);
         nameAppli.setTypeface(typeFace);
         descripAppli.setTypeface(typeFace);
+    }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.yourentry, menu);
+        return true;
     }
 }
