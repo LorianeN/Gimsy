ec92c8a5fecc0cf58b66794501b51b0be174f277
LorianeN <lnoiray@gmail.com>
2015-12-04 01:22:49 +0100

    plop

--- app/src/main/java/fred/gimsy/AccueilActivity.java
+++ app/src/main/java/fred/gimsy/AccueilActivity.java
@@ -1,7 +1,9 @@
 package fred.gimsy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+    }
+
+    public void selectLieux(View view) {
+        //TODO passer à l'activité suivante
+        Intent intent = new Intent(AccueilActivity.this,
+                Detail_liste.class);
+        intent.putExtra("","");
+        startActivity(intent);
     }
 }
